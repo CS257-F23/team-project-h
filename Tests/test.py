245203b1f7ca3bedfdac9eeba407b989206fb5d7
Alisa Ch <alisach@Alisas-MacBook-Pro.local>
2023-09-30 12:44:02 -0500

    added test class

---
+++ Tests/test.py
@@ -0,0 +1,17 @@
+from ProductionCode.stock_data import *
+import unittest
+import subprocess
+
+class Test():
+    def setUp(self):
+        load()
+
+    def test_get_all(self):
+        ''''''
+        #self.assertEquals(test_get_all(companyData), )
+
+    def test_get_by_date():
+        pass 
+
+    def test_get_by_company():
+        pass 

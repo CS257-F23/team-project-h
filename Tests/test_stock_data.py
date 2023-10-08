57c2f6600dbd4a263b2c0d8c245f92a1ccfb7f1c
CS Lab Account <cslab@olin310-21.mathcs.carleton.edu>
2023-10-08 17:20:48 -0500

    added more tests

--- Tests/test_stock_data.py
+++ Tests/test_stock_data.py
@@ -2,6 +2,7 @@ from ProductionCode.stock_data import *
 from test_data import *
 import unittest
 import subprocess
+import imp
 
 
 class TestBasic_cl(unittest.TestCase):
@@ -51,6 +52,8 @@ class TestBasic_cl(unittest.TestCase):
 
         self.assertEqual(data, filteredData)
 
+    def test_edge2_get_by_date(self):
+        self.assertEqual(None, get_by_date(self.testData, [1]))
 
     def test_print_data(self):
         """Test to check the data is printed correctly"""
@@ -68,6 +71,10 @@ class TestBasic_cl(unittest.TestCase):
         self.assertIn(expected_company2, output.strip())
 
         code.terminate()
+
+
+    def test_edge_print_data(self):
+        self.assertEquals(None, print_data(None))
     
 
     def test_get_all(self):
@@ -88,13 +95,53 @@ class TestBasic_cl(unittest.TestCase):
         '''This tests load on the the entire data set'''
 
         self.assertEqual(len(load()), 10)
+
+
+    def test_load_helper(self):
+        '''This tests a helper function for load'''
+        sampleData = ["2020-04-01,0,0,0,0"]
+        self.assertEqual(type(load_helper(sampleData)), dict)
         
+
+    def test_get_by_date_helper(self):
+        '''This tests a helper function for get_by_date()'''
+        filteredCompanyData = {}
+        filteredCompanyData["NFLX"] = {}
+        get_by_date_helper(self.testData, "NFLX", [1,1,"2020-04-01"], filteredCompanyData)
+        self.assertEqual(filteredCompanyData, {"NFLX":{'2020-04-01' : "111"}})
+
+
+    def test_edge_get_by_date_helper(self):
+        '''This tests a helper function edge case for get_by_date() - invalid date'''
+        filteredCompanyData = {}
+        filteredCompanyData["NFLX"] = {}
+        get_by_date_helper(self.testData, "NFLX", [1,1,"invalid_date"], filteredCompanyData)
+        self.assertEqual(filteredCompanyData, {"NFLX":{}})
+
 
     def test_get_help(self):
         '''Test to see if the get_help function runs '''
 
         self.assertEqual(get_help(), True)
 
+    def test_get_help_main(self):
+        self.assertEquals(None, main([1, "--get_h"]))
+
+    def test_get_all_main(self):
+        self.assertEquals(None, main([1, "--get_all"]))
+
+    def test_get_by_company_main(self):
+        self.assertEquals(None, main([1, "--get_by_company", ""]))
+
+    def test_get_by_date_main(self):
+        self.assertEquals(None, main([1, "--get_by_date"]))
+
+    def test_edge_main(self):
+        self.assertEqual(None, main([1]))
+    
+    def test_name_main(self):
+        #using the imp module because using import to load python functions was not working with the unittest module and coverage testing
+        runpy = imp.load_source('__main__', 'ProductionCode/stock_data.py')
 
 
 if __name__ == '__main__':
